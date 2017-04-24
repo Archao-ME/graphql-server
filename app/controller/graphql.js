4ce83930bdd4e1832960313bb721831d5ec35646
Jesusalexander <brpoper@gmail.com>
2017-04-24 23:13:56 +0800

    初步格式化

--- app/controller/graphql.js
+++ app/controller/graphql.js
@@ -1,45 +1,9 @@
 const Router = require('koa-router');
 const graphql = new Router();
-const {makeExecutableSchema, addErrorLoggingToSchema, SchemaError} = require('graphql-tools');
+const {makeExecutableSchema, addErrorLoggingToSchema} = require('graphql-tools');
 const {graphqlKoa, graphiqlKoa} = require('graphql-server-koa');
-const {getHello,changeWorld} = require('../service/hello');
-
-const typeDefs = `
-  type Hello {
-    id: ID
-    content: String
-  }
-  type Query {
-    hello: Hello
-  }
-  type Mutation {
-    changeWord(content: String!): Hello
-  }
-  schema {
-    query: Query
-    mutation: Mutation
-  }
-`;
-
-const resolvers = {
-  Query: {
-    async hello(root, args, context) {
-      const result = await getHello();
-      return result;
-    },
-  },
-  Mutation: {
-    changeWord(root,{content}){
-      if(content == 'world'){
-        return new SchemaError({
-          errorCode: 1223,
-          msg: '这个world已经存在了'
-        })
-      }
-      return changeWorld(content);
-    }
-  }
-};
+const typeDefs = require('../model/typeDefs');
+const resolvers = require('../model/resolvers');
 
 const schema = makeExecutableSchema({
   typeDefs,
